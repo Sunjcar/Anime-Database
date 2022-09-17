dcacc1fbd355b98440e3563f763a12cf28ca03d5
Raymond Chen <raychen358@gmail.com>
2022-09-16 22:18:00 -0700

    Sidebar api displays top 5 manga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import {React, useState} from 'react';
+import { React, useState, useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   const [mangaList, setMangaList] = useState([])
@@ -8,9 +9,24 @@ function App() {
   const [search, setSearch] = useState('')
 
 
+  const getTopManga = async () => {
+    const mangaAPI = await fetch (`https://api.jikan.moe/v4/top/manga`)
+      .then(res => res.json())
+
+      setTopManga(mangaAPI.data.slice(0,5))
+  }
+
+  useEffect(() => {
+    getTopManga();
+  }, [])
+
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div className='content-wrap'>
+        <Sidebar
+          topManga={topManga} />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Sidebar({ topManga }) {
+    return (
+        <aside>
+            <nav>
+                <h3>Top Manga</h3>
+                {topManga.map(manga => (
+                    <a
+                        href={manga.url}
+                        target='_blank'
+                        rel='noreferrer'
+                        key={manga.mal_id}
+                    >
+                        {manga.title}
+                        <figure>
+                            <img
+                                src={manga.images.jpg.small_image_url} alt='Anime' />
+                        </figure>
+                    </a>
+                ))}
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar
